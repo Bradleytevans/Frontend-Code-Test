9f50d65cf0cad80c1e5fa50bab67612e3c2eeadb
bradleytevans <bradleyt.evans@gmail.com>
2022-05-07 19:28:08 -0400

    adding functionality to buttons

--- ProblemTwo/iphone-calculator/src/components/Button/index.js
+++ ProblemTwo/iphone-calculator/src/components/Button/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import "./Button.css";
 
-const Button = ({content}) => {
-    return (<div className={`Button ${content === '0' ? 'zero' : ""}`}>{content}</div>);
+const Button = ({content, onButtonClick, type }) => {
+    return (<div className={`Button ${content === '0' ? 'zero' : ""} ${type || ""}`} onClick={onButtonClick(content)}>{content}</div>);
 };
 
 export default Button;
