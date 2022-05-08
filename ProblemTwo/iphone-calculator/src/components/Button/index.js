9245e08e9fe12087d11b2577c8dec014599c8f0b
bradleytevans <bradleyt.evans@gmail.com>
2022-05-08 01:48:21 -0400

    added commas and a clock

--- ProblemTwo/iphone-calculator/src/components/Button/index.js
+++ ProblemTwo/iphone-calculator/src/components/Button/index.js
@@ -1,9 +1,17 @@
+
 import React from "react";
 
 import "./Button.css";
 
-const Button = ({content, onButtonClick, type }) => {
-    return (<div className={`Button ${content === '0' ? 'zero' : ""} ${type || ""}`} onClick={onButtonClick(content)}>{content}</div>);
+const Button = ({ content, onButtonClick, type }) => {
+  return (
+    <div
+      className={`Button ${content === "0" ? "zero" : ""} ${type || ""}`}
+      onClick={onButtonClick(content)}
+    >
+      {content}
+    </div>
+  );
 };
 
 export default Button;
